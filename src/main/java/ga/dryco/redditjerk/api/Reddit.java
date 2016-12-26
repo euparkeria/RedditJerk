a1f2f0b29eee2b5de3998f261d453b833d90d663
euparkeria <atamagahennaneko@gmail.com>
2016-12-26 18:43:19 +0200

    test

--- src/main/java/ga/dryco/redditjerk/api/Reddit.java
+++ src/main/java/ga/dryco/redditjerk/api/Reddit.java
@@ -1,24 +1,12 @@
 package ga.dryco.redditjerk.api;
 
 
-import ga.dryco.redditjerk.wrappers.*;
 
-import java.util.List;
 
 /**
  * Parent Reddit Api Interface
  */
 public interface Reddit extends FlairApi, ModerationApi, PostApi, SubredditApi, UserApi {
-
-
-
-
-
-
-
-
-
-
 
 
 
